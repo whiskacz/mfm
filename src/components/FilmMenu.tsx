daab2b82373584bc0fbe604b0849dff1344c0fc6
whiskacz <pawelblasiak1985@gmail.com>
2023-11-05 23:00:21 +0100

    core searchbar api

--- src/components/FilmMenu.tsx
+++ src/components/FilmMenu.tsx
@@ -3,10 +3,13 @@ import '../mainCSS/mainCSS.css'
 
 export const FilmMenu = () => {
   return (
-    <main className='mainFilmWrapper'>
-        <div>
-          <label htmlFor="filmSearch">Enter film name</label>  
-          <input type="text" id='filmSearch' />
+    <main className='FilmMenuWrapper'>
+        <div className='filmInput moduls'>
+          <input type="text" />
+          <label>Enter film name</label>  
+        </div>
+        <div className='filmList moduls'>
+        zzzzzzzzzzzzzz
         </div>
 
     </main>
